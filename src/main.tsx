5a9fb2b5f449c1720583806268fb48d2049ce80c
jwrigh26 <jwrigh26@gmail.com>
2025-03-30 11:29:31 -0600

    firebase ver 1

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,20 @@ import ReactDOM from 'react-dom/client';
 import { routeTree } from './routeTree.gen';
 import { queryClient } from './utils/queryClient';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_REACT_APP_FB_API_URL,
+  authDomain: import.meta.env.VITE_REACT_APP_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_FB_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_APP_FB_MEASUREMENT_ID
+};
+
+
 interface MyRouterContext {
   user?: string;
 }
