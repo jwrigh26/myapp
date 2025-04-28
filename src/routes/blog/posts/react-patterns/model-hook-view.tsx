74da5dca64df04c958d1323079880989475d5434
jwrigh26 <jwrigh26@gmail.com>
2025-04-27 22:02:59 -0600

    mvh wip2

--- src/routes/blog/posts/react-patterns/model-hook-view.tsx
+++ src/routes/blog/posts/react-patterns/model-hook-view.tsx
@@ -35,7 +35,9 @@ function RouteComponent() {
         title="Model Hook View (MHV)"
         preSubtitle="A React pattern for managing components using hooks as controllers"
       />
-      <TitleBlock subtitle="Writing about what I use every day">Sticking with React 18</TitleBlock>
+      <TitleBlock subtitle="Writing about what I use every day">
+        Sticking with React 18
+      </TitleBlock>
       <IntroBlock>
         It's April 2025, and I'm writing my first React tutorial using React 18.
         This seems a little odd since React 19 is already out, complete with new
@@ -49,16 +51,18 @@ function RouteComponent() {
         <ProseBlock>
           Another reason is I'm not yet sure how I feel about React 19. I don't
           have enough experience with it to form a strong opinion. It's probably
-          fine, but I need more hands-on time to be sure. Honestly speaking, for
-          my personal projects, I might even explore other JavaScript
-          frameworks. Time will tell. Until then, I thought it would be fun to
-          start writing about things I've learned as a React developer since
-          2017.
+          fine, but I need more hands-on time to be sure. Maybe once I have more
+          experience with it, I'll come back and update this post to use React
+          19. Until then, I thought it would be fun to start writing about what
+          I know and things I've learned as a React developer since 2017.
         </ProseBlock>
       </Box>
       <BodyBlock>
         <SectionSpacer id="model-hook-controller" />
-        <ProseBlock title="Introducing Model Hook View" subtitle="My spin on a common design pattern" />
+        <ProseBlock
+          title="Introducing Model Hook View"
+          subtitle="My spin on a common design pattern"
+        />
         <ProseBlock>
           After careful consideration, my first React post will cover what I've
           dubbed the "Model Hook View" strategy.
@@ -71,31 +75,13 @@ function RouteComponent() {
           similar to the Model Hook View (MHV) strategy.
         </DisclaimerBlock>
         <ProseBlock>
-          The problem I've encountered is twofold:
-          <ol>
-            <li>
-              When casually searching the web for similar concepts, I've found
-              some examples. However, they're either presented in ways I can't
-              relate to, or the sites are packed with ads and poorly written
-              content designed purely for SEO, resulting in an unpleasant
-              reading experience.
-            </li>
-            <li>
-              Even though this concept might be common, I've seen many React
-              developers (myself included) simply toss everything into a JSX
-              component and call it a day. There's nothing inherently wrong with
-              this approach if the component or logic stays manageable. But what
-              happens when your component gets complex?
-            </li>
-          </ol>
+          While I know this design pattern isn't new, I wanted to document it
+          because learning and actually applying the MHV design pattern can be
+          helpful. So without further rambling, let's dive into it.
         </ProseBlock>
-        <ProseBlock>
-          That's when I believe learning and actually applying the MHV design
-          pattern can be helpful. So without further rambling, let's dive into
-          it.
-        </ProseBlock>
-        <NoteBlock variant='warning'>
-          Work in progress...Will be slowly updating this post over the next few weeks.
+        <NoteBlock variant="warning">
+          Work in progress...Will be slowly updating this post over the next few
+          weeks.
         </NoteBlock>
       </BodyBlock>
     </>
